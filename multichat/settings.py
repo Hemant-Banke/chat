bb13a5df54ce6fbeda816fd7b1976169f1e5d842
Hemant Banke <hemantbanke5@gmail.com>
2020-05-20 02:37:28 +0530

    Added Whitenoise

--- multichat/settings.py
+++ multichat/settings.py
@@ -101,6 +101,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'multichat.urls'
@@ -163,3 +164,4 @@ STATIC_ROOT = 'static'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
